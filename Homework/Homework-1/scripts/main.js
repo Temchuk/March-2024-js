0f9c8cd1bc482bdd474d4fe7748ad10935c1c3eb
Temchuk <too060802@gmail.com>
2024-04-30 03:00:43 +0300

    done hw1

---
+++ Homework/Homework-1/scripts/main.js
@@ -0,0 +1,66 @@
+// 'use strict';
+
+// --------------Завдання 1------------
+
+let x1 = 'hello';
+let x2 = 'owu';
+let x3 = 'com';
+let x4 = 'ua';
+let x5 = 1;
+let x6 = 10;
+let x7 = -999;
+let x8 = 123;
+const PI = 3.14;
+let x10 = 2.7;
+let x11 = 16;
+let x12 = true;
+let x13 = false;
+
+
+
+console.log(x1);
+console.log(x2);
+console.log(x3);
+console.log(x4);
+console.log(x5);
+console.log(x6);
+console.log(x7);
+console.log(x8);
+console.log(PI);
+console.log(x10);
+console.log(x11);
+console.log(x12);
+console.log(x13);
+
+
+// --------------Завдання 2------------
+
+let firstName = 'Olena';
+let middleName = 'Olehivna';
+let lastName = 'Temchuk';
+
+let space = ' ';
+let person = firstName + space + middleName + space + lastName;
+console.log(person);
+
+result = `${firstName} ${middleName} ${lastName}`;
+console.log(result);
+
+// --------------Завдання 3------------
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// --------------Завдання 4------------
+
+let fName = prompt();
+let mName = prompt();
+let age = prompt();
+
+result2 = `${fName} ${mName} ${age}`;
+console.log(result2);
